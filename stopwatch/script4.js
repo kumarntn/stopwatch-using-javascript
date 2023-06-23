6de81f43fc46d8c675de1ee09bc3b810628f4a71
NARAYANAKUMAR T <137101298+kumarntn@users.noreply.github.com>
2023-06-23 20:52:27 +0530

    Add files via upload

---
+++ stopwatch/script4.js
@@ -0,0 +1,61 @@
+const btnStart = document.querySelector('.start');
+const btnStop = document.querySelector('.stop');
+const btnReset = document.querySelector('.reset');
+
+let hrs = min = sec = ms = 0, startTimer;
+
+btnStart.addEventListener('click', () => {
+    btnStart.classList.add('start-active');
+    btnStop.classList.remove('stop-active');
+    startTimer = setInterval(() => {
+        ms++;
+        if (ms == 100) {
+            sec++;
+            ms = 0;
+
+        }
+        if (sec == 60) {
+            min++;
+            sec = 0;
+
+        }
+        if (min == 60) {
+            hrs++;
+            min = 0;
+
+        }
+
+
+
+        updateDisplay();
+    }, 10);
+});
+btnStop.addEventListener('click', () => {
+    clearInterval(startTimer);
+
+    btnStart.classList.remove('start-active');
+    btnStop.classList.add('stop-active');
+});
+btnReset.addEventListener('click', () => {
+    hrs = min = sec = ms = 0;
+    clearInterval(startTimer);
+    updateDisplay();
+
+    btnStart.classList.remove('start-active');
+    btnStop.classList.remove('stop-active');
+});
+
+function updateDisplay() {
+    //formated Display
+    phrs = hrs < 10 ? '0' + hrs : hrs;
+    pmin = min < 10 ? '0' + min : min;
+    psec = sec < 10 ? '0' + sec : sec;
+    pms = ms < 10 ? '0' + ms : ms;
+    //output
+    document.querySelector('.hrs').innerText = hrs;
+    document.querySelector('.min').innerText = min;
+    document.querySelector('.sec').innerText = sec;
+    document.querySelector('.ms').innerText = ms;
+
+
+}
